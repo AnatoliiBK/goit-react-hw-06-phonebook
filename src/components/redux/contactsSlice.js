9a70d5204290fa83f9626a0c088e9033bae7aad7
AnetRo <beatrice.ccq@gmail.com>
2023-07-14 00:37:38 +0300

    Update contactsSlice.js

--- src/components/redux/contactsSlice.js
+++ src/components/redux/contactsSlice.js
@@ -9,8 +9,33 @@ const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
   reducers: {
+    // addContact: (state, action) => {
+    //   state.contacts.push(action.payload);
+    // },
+    // addContact: (state, action) => {
+    //   const { id, name, number } = action.payload;
+    //   const duplicateContact = state.contacts.find(
+    //     (contact) => contact.name === name && contact.number === number
+    //   );
+    //   if (!duplicateContact) {
+    //     state.contacts.push({ id, name, number });
+    //   } 
+    // },
     addContact: (state, action) => {
-      state.contacts.push(action.payload);
+      const { id, name, number } = action.payload;
+
+      if (!name || !number) {
+        return;
+      }  
+
+      const duplicateContact = state.contacts.find(
+        (contact) => contact.number === number
+      );
+      if (duplicateContact) {
+        alert(`Контакт з номером телефону ${number} вже є в списку`);
+        return;
+      }
+      state.contacts.push({ id, name, number });
     },
     deleteContact: (state, action) => {
       state.contacts = state.contacts.filter(
